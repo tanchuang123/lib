606b3b6e9f8cb06fb0bf661c1e0aef221407356c
tanchuang <tanchuang@seer-robotics.com>
2018-01-29 13:10:05 +0800

    库

---
+++ plus/plus.cpp
@@ -0,0 +1,13 @@
+#include "plus.h"
+
+
+Plus::Plus()
+{
+}
+int Plus::addFun(int a,int b)
+
+{
+
+ return a+b;
+
+}
---
+++ plus/plus.h
@@ -0,0 +1,14 @@
+#ifndef PLUS_H
+#define PLUS_H
+
+#include "plus_global.h"
+
+class PLUSSHARED_EXPORT Plus
+{
+
+public:
+    Plus();
+    int addFun(int a,int b);
+};
+
+#endif // PLUS_H
